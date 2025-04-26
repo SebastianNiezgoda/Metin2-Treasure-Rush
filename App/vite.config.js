5d78c20a1bc0c874230e1e01eaa2591eca0c7bf2
SebastianNiezgoda <sebastian.niezgoda12@gmail.com>
2025-04-26 17:14:20 +0200

    .

--- App/vite.config.js
+++ App/vite.config.js
@@ -1,9 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public', 
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    open: true,
+    port: 5173,
+  },
+});
